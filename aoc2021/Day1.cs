5931b2b3fdbfdeaef25848fe0627d10eb90186ac
annabru <anna95b7@gmail.com>
2021-12-04 12:15:53 +0100

    Init commit

---
+++ aoc2021/Day1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2021
+{
+    public static class Day1
+    {
+
+        public static void CountIncreases()
+        {
+
+        }
+    }
+}
